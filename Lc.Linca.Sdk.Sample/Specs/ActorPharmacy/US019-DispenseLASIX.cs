82661bca3c968bede2622b348d771683529664eb
annemarie.goldmann <annemarie.goldmann@steszgal.at>
2025-01-17 10:41:42 +0100

    #-wip

--- Lc.Linca.Sdk.Sample/Specs/ActorPharmacy/US019-DispenseLASIX.cs
+++ Lc.Linca.Sdk.Sample/Specs/ActorPharmacy/US019-DispenseLASIX.cs
@@ -16,7 +16,7 @@ namespace Lc.Linca.Sdk.Specs.ActorPharmacy;
 
 internal class US019_DispenseLasix : Spec
 {
-    protected MedicationDispense dispense1 = new();
+    
 
     public const string UserStory = @"
         Pharmacist Mag. Franziska Fröschl, owner of the pharmacy Apotheke 'Klappernder Storch' has 
@@ -37,97 +37,86 @@ internal class US019_DispenseLasix : Spec
 
     private bool CreateMedicationDispenseRecord()
     {
-        LinkedCareSampleClient.CareInformationSystemScaffold.PseudoDatabaseRetrieve();
+        //LinkedCareSampleClient.CareInformationSystemScaffold.PseudoDatabaseRetrieve();
 
-        (Bundle orders, bool received) = LincaDataExchange.GetPrescriptionToDispense(Connection, "1111BBBB3333");
+        (Bundle orders, bool received) = LincaDataExchange.GetPrescriptionToDispense(Connection, " ");
 
         if (received)
         {
             List<MedicationRequest> prescriptionsToDispense = BundleHelper.FilterPrescriptionsToDispense(orders);
 
-            MedicationRequest? prescriptionLasix = prescriptionsToDispense.Find(x => x.Medication.Concept.Coding.First().Code.Equals("0031130"));
+            // MedicationRequest? prescriptionLasix = prescriptionsToDispense.Find(x => x.Medication.Concept.Coding.First().Code.Equals("0031130"));
 
-            if (prescriptionLasix == null)
+            if (prescriptionsToDispense.Count != 5)
             {
-                Console.WriteLine("Linca PrescriptionMedicationRequest for Gertrude Steinmaier not found, LINCAMedicationDispense cannot be created");
+                Console.WriteLine("Linca PrescriptionMedicationRequests not correct, LINCAMedicationDispense cannot be created");
 
                 return (false);
             }
 
-            dispense1.AuthorizingPrescription.Add(new()
-            {
-                Reference = $"LINCAPrescriptionMedicationRequest/{prescriptionLasix.Id}"
-            });
+            var dispenseCount = 0;
 
-            dispense1.Status = MedicationDispense.MedicationDispenseStatusCodes.Completed;
-            dispense1.Subject = prescriptionLasix.Subject;
-            
-            dispense1.Medication = new()
+            foreach (var prescription in prescriptionsToDispense)
             {
-                Concept = new()
+                MedicationDispense dispense1 = new();
+
+                dispense1.AuthorizingPrescription.Add(new()
                 {
-                    Coding = new()
+                    Reference = $"LINCAPrescriptionMedicationRequest/{prescription.Id}"
+                });
+
+                dispense1.Status = MedicationDispense.MedicationDispenseStatusCodes.Completed;
+                dispense1.Subject = prescription.Subject;
+
+                dispense1.Medication = prescription.Medication;
+
+                dispense1.Quantity = prescription.DispenseRequest.Quantity;
+
+                dispense1.DosageInstruction = prescription.DosageInstruction;
+
+
+                dispense1.Performer.Add(new()
+                {
+                    Actor = new()
                     {
-                        new Coding()
+                        Identifier = new()
                         {
-                            Code = "1336328",
-                            System = "https://termgit.elga.gv.at/CodeSystem/asp-liste",
-                            Display = "LASIX TBL 80MG"
+                            Value = "1.2.40.0.34.3.1.11173",  // OID of dispensing pharmacy
+                            System = "urn:ietf:rfc:3986"  // Code-System: eHVD
                         }
                     }
-                }
-            };
+                });
 
-            dispense1.Quantity = new() { Value = 1 };
-
-            dispense1.DosageInstruction = prescriptionLasix?.DosageInstruction;
-
-            dispense1.Note = new()
-            {
-                new() { Text = "Achtung: höherer Wirkstoffgehalt, Dosierung anpassen: 1/2-0-0-0" }
-            };
-
-            dispense1.Performer.Add(new()
-            {
-                Actor = new()
+                dispense1.Type = new()
                 {
-                    Identifier = new()
-                    {
-                        Value = "2.999.40.0.34.5.1.1",  // OID of dispensing pharmacy
-                        System = "urn:ietf:rfc:3986"  // Code-System: eHVD
-                    },
-                    Display = "Apotheke 'Klappernder Storch'"
-                }
-            });
-
-            dispense1.Type = new()
-            {
-                Coding = new()
+                    Coding = new()
                 {
                     new Coding(system: "http://terminology.hl7.org/CodeSystem/v3-ActCode", code: "FFC") // complete the dispense
                 }
-            };
+                };
 
-            (var postedMD, var canCue, var outcome) = LincaDataExchange.CreateMedicationDispense(Connection, dispense1);
+                (var postedMD, var canCue, var outcome) = LincaDataExchange.CreateMedicationDispense(Connection, dispense1);
 
-            if (canCue)
-            {
-                Console.WriteLine($"Linca MedicationDispense transmitted, id {postedMD.Id}");
-            }
-            else
-            {
-                Console.WriteLine($"Failed to transmit Linca MedicationDispense");
-            }
-
-            if (outcome != null)
-            {
-                foreach (var item in outcome.Issue)
+                if (canCue)
                 {
-                    Console.WriteLine($"Outcome Issue Code: '{item.Details.Coding?.FirstOrDefault()?.Code}', Text: '{item.Details.Text}'");
+                    Console.WriteLine($"Linca MedicationDispense transmitted, id {postedMD.Id}");
+                    dispenseCount++;
+                }
+                else
+                {
+                    Console.WriteLine($"Failed to transmit Linca MedicationDispense");
+                }
+
+                if (outcome != null)
+                {
+                    foreach (var item in outcome.Issue)
+                    {
+                        Console.WriteLine($"Outcome Issue Code: '{item.Details.Coding?.FirstOrDefault()?.Code}', Text: '{item.Details.Text}'");
+                    }
                 }
             }
 
-            return canCue;
+            return dispenseCount == 5;
         }
         else
         {
